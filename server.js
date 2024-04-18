77df7d390ac96653fd0d1b8cfdadd2ec379594b0
KaoDje <antoine.tessier.si@gmail.com>
2024-04-18 10:46:24 +0200

    Adding route test to server.js

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const port = 3000;
 const app = express();
 
 app.use(express.json);
+
+app.use("/test", testRouter);
+
+app.use((error, req, res, next) => {
+  res
+    .status(error.status || 500)
+    .json({ success: false, error: error.message, status: error.status });
+});
+
+app.listen(port, hostname, () => {
+  console.log("server is running on port " + port);
+});
